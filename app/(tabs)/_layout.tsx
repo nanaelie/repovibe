bd96a24dfad26f5a80f161e4bd25dbd4cb60be01
nanaelie <elienana92@gmail.com>
2025-06-11 19:12:51 +0000

    chore: configuration de la barre de navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
+import { ThemeProvider } from '@/contexts/ThemeContext'; // <<---- Ajouté
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -12,34 +13,37 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <ThemeProvider>
+      <Tabs
+		  screenOptions={{
+			tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+			headerShown: false,
+			tabBarButton: HapticTab,
+			tabBarBackground: TabBarBackground,
+			tabBarStyle: {
+			  backgroundColor: '#0A0A0A',
+			  position: Platform.OS === 'ios' ? 'absolute' : 'relative',
+			  borderTopWidth: 0,
+			  elevation: 0,
+			  paddingBlock: 5,
+			},
+		  }}
+		>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Repos',
+            tabBarIcon: () => <IconSymbol size={30} name="house.fill" color='#424242' />,
+          }}
+        />
+        <Tabs.Screen
+          name="user"
+          options={{
+            title: 'User',
+            tabBarIcon: () => <IconSymbol size={30} name="account.circle" color='#424242' />,
+          }}
+        />
+      </Tabs>
+    </ThemeProvider>
   );
 }
